1003abe64336b817a0657d5671b063cdc8f9e91b
Jang DongHyeon <jdh3876@naver.com>
2022-07-27 18:41:22 +0900

    Update complimentaryFilter (ATMega 328).ino

--- src/Air Mouse (with UNO Board)/complimentaryFilter (ATMega 328).ino
+++ src/Air Mouse (with UNO Board)/complimentaryFilter (ATMega 328).ino
@@ -1,6 +1,10 @@
 #include <Wire.h>
 
+// MPU 6050의 I2C 기본 주소
 const int MPU_ADDR = 0x68;
+
+// MPU 6050에 저장되는 각속도 데이터는 unsigned 2Byte 데이터 이므로 0 ~ 65535 사이의 값으로 저장된다.
+// MPU 6050의 기본적인 각속도 측정 범위는 -250 º/s ~ +250 º/s 이다.
 const double raw_to_gyro = 32767 / 250;
 
 int16_t gyX, gyY, gyZ;
@@ -29,11 +33,12 @@ void loop() {
 //  Serial.print("Y축 각속도 : "); Serial.print(gyY_cal); Serial.print("\t");
 //  Serial.print("Z축 각속도 : "); Serial.println(gyZ_cal);
 
+  // 하드웨어 시리얼로 ATMega 16u2에 x축, z축 각속도 데이터를 전송한다.
   Serial.print(gyX_cal); Serial.print(",");
   Serial.println(gyZ_cal);
-
 }
 
+// MPU 6050과의 IC2 연결을 시작하고, 센서를 활성화 시키는 함수이다.
 void setting() {
    Wire.begin();
    Wire.beginTransmission(MPU_ADDR);
@@ -42,6 +47,7 @@ void setting() {
    Wire.endTransmission();
 }
 
+// MPU 6050으로부터 x, y, z축 각속도 데이터를 받아오는 함수이다.
 void getData() {
   Wire.beginTransmission(MPU_ADDR);
   Wire.write(0x43);
@@ -51,9 +57,10 @@ void getData() {
   gyY = Wire.read() << 8 | Wire.read();
   gyZ = Wire.read() << 8 | Wire.read();
   Wire.endTransmission();
-
 }
 
+// MPU 6050을 처음 활성화 시켰을 때 초기 잡음이 있을 수 있다.
+// 10개 각속도 데이터의 평균을 구하여 초기 잡음으로 취급한다.
 void ave_cal() {
   for (int i=0; i<10; i++) {
     getData();
